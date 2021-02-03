1d4ad72a0be3e1409e74a20269f42ea8c1988985
Martin Muggli <martin.muggli@gmail.com>
2021-02-03 08:58:12 -0700

    Convert a word list on disk to something that we can build an inverted index on

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
 import Lib
+import System.IO
+import Data.List
+
+enumerate :: String -> [(Char, Int)]
+enumerate s = zip s [1..]
+
+wordify :: String -> [(Char, Int)]
+wordify s = let groups = group $ sort s in
+              concat $ map enumerate groups
+
+                
 
 main :: IO ()
-main = someFunc
+main = do
+  myfile <- openFile "/home/muggli/count_1w.txt" ReadMode
+  contents <- hGetContents myfile
+  let mylines = lines contents
+  let mywords = map  (head . words ) mylines
+  putStrLn $show $ wordify $ head mywords
+  hClose myfile
